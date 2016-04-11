abd7a54bc870556401ac147d1f64c8df47e33c68
AG <aaron.greenville@sydney.edu.au>
2016-04-11 17:18:09 +1000

    Modification of ascom driver from SGL Observatory Automation yahoo group.

---
+++ CloudSensorAaron/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ASCOM CloudSensor.SafetyMonitor")>
+<Assembly: AssemblyDescription("ASCOM SafetyMonitor driver for CloudSensor")>
+<Assembly: AssemblyCompany("ASCOM Initiative")>
+<Assembly: AssemblyProduct("ASCOM SafetyMonitor driver for CloudSensor")>
+<Assembly: AssemblyCopyright("Copyright © 2016, The ASCOM Intiative")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(True)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("3f8a0394-f2e5-4a6d-ba06-b0a41260305f")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("6.2.0.0")>
+<Assembly: AssemblyFileVersion("6.2.0.0")>
---
+++ CloudSensorAaron/SetupDialogForm.vb
@@ -0,0 +1,67 @@
+Imports Driver
+
+<ComVisible(False)>
+Public Class SetupDialogForm
+    Friend s As String
+    Friend safeState As Boolean = False
+    Friend objSerial As ASCOM.Utilities.Serial
+    Friend sky As String
+
+
+
+
+    Private Sub OK_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK_Button.Click ' OK button event handler
+        ' Persist new values of user settings to the ASCOM profile
+        SafetyMonitor.comPort = ComboBoxComPort.SelectedItem ' Update the state variables with results from the dialogue
+        SafetyMonitor.traceState = chkTrace.Checked
+        Me.DialogResult = System.Windows.Forms.DialogResult.OK
+        Me.Close()
+    End Sub
+
+    Private Sub Cancel_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel_Button.Click 'Cancel button event handler
+        Me.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Close()
+    End Sub
+
+    Private Sub ShowAscomWebPage(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox1.DoubleClick, PictureBox1.Click
+        ' Click on ASCOM logo event handler
+        Try
+            System.Diagnostics.Process.Start("http://ascom-standards.org/")
+        Catch noBrowser As System.ComponentModel.Win32Exception
+            If noBrowser.ErrorCode = -2147467259 Then
+                MessageBox.Show(noBrowser.Message)
+            End If
+        Catch other As System.Exception
+            MessageBox.Show(other.Message)
+        End Try
+    End Sub
+
+    Private Sub SetupDialogForm_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load ' Form load event handler
+        ' Retrieve current values of user settings from the ASCOM Profile
+        InitUI()
+    End Sub
+
+    Private Sub InitUI()
+        chkTrace.Checked = SafetyMonitor.traceState
+        ' set the list of com ports to those that are currently available
+        ComboBoxComPort.Items.Clear()
+        ComboBoxComPort.Items.AddRange(System.IO.Ports.SerialPort.GetPortNames())       ' use System.IO because it's static
+        ' select the current port if possible
+        If ComboBoxComPort.Items.Contains(SafetyMonitor.comPort) Then
+            ComboBoxComPort.SelectedItem = SafetyMonitor.comPort
+        End If
+    End Sub
+    Private Sub ComboBoxComPort_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBoxComPort.SelectedIndexChanged
+
+    End Sub
+
+    Public Sub TextAmbTemp_TextChanged(sender As Object, e As EventArgs) Handles TextAmbTemp.TextChanged
+        Timer1.Start()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If objSerial.Connected = True Then
+            TextAmbTemp.Text = sky
+        End If
+    End Sub
+End Class
